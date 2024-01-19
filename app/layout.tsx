04209ea13bf1aa9bcd91551d3e90be18260dbb38
HWG\khoabui <khoabui@hayworkglobal.com>
2024-01-19 11:05:52 +0700

    commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,38 +2,38 @@ import { GeistSans } from 'geist/font/sans';
 import './globals.css';
 import { Providers } from '@/lib/providers';
 import dynamic from 'next/dynamic';
-import 'antd/dist/reset.css';
 import { ConfigProvider } from 'antd';
 
 const Header = dynamic(() => import('@/components/header/Header'), {
-  ssr: false,
+	ssr: false,
 });
 
 const defaultUrl = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
-  : 'http://localhost:3000';
+	? `https://${process.env.VERCEL_URL}`
+	: 'http://localhost:3000';
 
 export const metadata = {
-  metadataBase: new URL(defaultUrl),
-  title: 'Bong da suc khoe toi',
-  description: 'The fastest way to build apps with Next.js and Supabase',
+	metadataBase: new URL(defaultUrl),
+	title: 'Bong da suc khoe toi',
+	description: 'The fastest way to build apps with Next.js and Supabase',
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-  return (
-    <Providers>
-      <html lang="en" className={GeistSans.className}>
-			<ConfigProvider><body>
-          <Header />
-				 <main className="-z-10">{children}</main>
-         
-        </body>
-				</ConfigProvider>
-      </html>
-    </Providers>
-  );
+	return (
+		<ConfigProvider theme={{ hashed: false }}>
+			<Providers>
+				<html lang="en" className={GeistSans.className}>
+					<body>
+						<Header />
+						<main className="-z-10">{children}</main>
+
+					</body>
+				</html>
+			</Providers>
+		</ConfigProvider>
+	);
 }
